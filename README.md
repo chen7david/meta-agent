281655bb7b72be8c0029758d371822c84fb77abc
David <chen7david@me.com>
2020-10-18 22:40:51 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # meta-agent
+### Getting Started
+
+```js
+const meta = require('./meta')({
+    baseURL: 'http://hostname:8000',
+})
+
+const someAsyncFunction = async () => {
+    /* MOVIES */
+    const allCachedMovies = await meta.movies().all()
+    const searchMoviesOnTMDB = await meta.tmdb().movies().search('some-movie-name', {year:2007})
+    const searchMoviesOnCache = await meta.movies().search('some-movie-name', {year:2007})
+    const importMovieFromTMDB = await meta.tmdb().movies().import('some-movie-id')
+    const getMovieById = await meta.movies().withId('some-movie-id')
+    const deleteMovieById = await meta.movies().delete('some-movie-id')
+
+    /* SHOWS */
+    const allCachedShows = await meta.shows().all()
+    const searchShowsOnTMDB = await meta.tmdb().shows().search('some-show-name', {year:2007})
+    const searchShowsOnCache = await meta.shows().search('some-show-name', {year:2007})
+    const importShowFromTMDB = await meta.tmdb().shows().import('some-show-id')
+    const getShowById = await meta.shows().withId('some-show-id')
+    const updateShowSeasonById = await meta.shows().update('some-show-id', 'some-season-number')
+    const deleteShowById = await meta.shows().delete('some-show-id')
+
+    /* TRENDING */
+    const allTrendingMovies = await meta.moives().trending({window:'day'})
+    const allTrendingShows = await meta.shows().trending({window:'day'})
+    const allTrendingPeople = await meta.people().trending({window:'week'})
+
+    /* GENRES */
+    const allMovieGenres = await meta.moives().genres()
+    const allShowGenres = await meta.shows().genres()
+    const allGenres = await meta.genres({type: 'all'})
+}
+```
---
+++ index.js
@@ -0,0 +1,93 @@
+
+
+class Meta {
+    constructor(options){
+        this.http = require('./http')(options)
+        this.state = { url: '', params: {}}
+    }
+
+    tmdb(){
+        if(this.state.url.length > 0) throw(`tmdb() should be called first in the chain`)
+        this.state.url = '/tmdb-'
+        return this
+    }
+
+    movies(){
+        this.state.url += this.state.url.length > 0 ? 'movies' : '/movies'
+        return this
+    }
+
+    shows(){
+        this.state.url += this.state.url.length > 0 ? 'shows' : '/shows'
+        return this
+    }
+
+    people(){
+        this.state.url += this.state.url.length > 0 ? 'people' : '/people'
+        return this
+    }
+
+    async import(id){
+        if(!id) throw(`id should be an integer ${id} was given`)
+        this.state.url += '/' + id
+        return await this.http.get(this.state.url)
+    }
+
+    async update(id, season){
+        if(!id || !season) throw(`invalid parameter value(s)`)
+        this.state.url += `/${id}/season/${season}`
+        return await this.http.patch(this.state.url)
+    }
+
+    async search(name, params = {}){
+        params.search = name
+        let url = ''.concat(this.state.url, '?', this.toQueryString(params))
+        return await this.http.get(url)
+    }
+
+    async trending(params = {}){
+        params.type = this.state.url.split('/')[1]
+        let url = '/trending'.concat('?', this.toQueryString(params))
+        return await this.http.get(url)
+    }
+
+    async genres(params = {}){
+        params.type = params.type ? '' : this.state.url.split('/')[1] 
+        let url = '/genres'.concat('?', this.toQueryString(params))
+        return await this.http.get(url)
+    }
+
+    async withId(id, params = {}){
+        if(id == null) throw(`id is required but ${id} was given!`)
+        let url = ''.concat(this.state.url,'/', id, this.toQueryString(params))
+        return await this.http.get(url)
+    }
+
+    async delete(id){
+        if(id == null) throw(`id is required but ${id} was given!`)
+        let url = ''.concat(this.state.url,'/', id)
+        return await this.http.delete(url)
+    }
+
+    async all(){
+        return await this.http.get(this.state.url)
+    }
+
+    toQueryString(params = {}){
+        return Object.keys(Object.assign(params, this.state.params))
+            .map(key => key + '=' + params[key]).join('&')
+    }
+}
+
+exports = module.exports = (options = {}) => {
+    if(!options.baseURL) throw('baseURL required!')
+    return new Meta(options)
+}
+
+exports.meta = (options = {}) => async (ctx, next) => {
+    if(!options.baseURL) throw('baseURL required!')
+    ctx.meta = new Meta(options) 
+    await next()
+}
+
+exports.Meta = Meta
